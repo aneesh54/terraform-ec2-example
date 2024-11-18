a0a3bc7fbfe8facb61ff5bb935b8834184abfac5
Aneesh pandey <aneeshp60@gmail.com>
2024-11-18 11:42:36 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,1 +1,15 @@
+variable "aws_region" {
+  default     = "us-east-1"
+  description = "AWS region for resources"
+}
+
+variable "instance_type" {
+  default     = "t2.micro"
+  description = "Instance type for EC2"
+}
+
+variable "ami_id" {
+  default     = "ami-0c02fb55956c7d316" # Replace with a valid AMI ID
+  description = "AMI ID for the instance"
+}
 
